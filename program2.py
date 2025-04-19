a6afb7cd94747e3b23a931e2ba31b8409332ace6
karolinaowczarek-hub <karolinaowczarek100@gmail.com>
2025-04-19 11:22:11 +0200

    Delete program2.py

--- program2.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://jsonplaceholder.typicode.com/todos/1"
-response = requests.get(url)
-
-with open("response.json", "w") as file:
-    file.write(response.text)
-
-print("Response saved to response.json")
