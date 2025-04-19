3d614b57382834899714f8f6a6fc1dc240c44f97
karolinaowczarek-hub <karolinaowczarek100@gmail.com>
2025-04-19 11:22:26 +0200

    Delete program1.py

--- program1.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://jsonplaceholder.typicode.com/todos/1"
-response = requests.get(url)
-
-print("Status code:", response.status_code)
-print("Response JSON:", response.json())
